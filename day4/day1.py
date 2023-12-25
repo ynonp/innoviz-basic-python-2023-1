197e5086f81ba7a17a8fb0fdce0cdcf1263551fc
ynonp <ynonperek@gmail.com>
2023-12-25 11:06:50 +0200

    add instructions to day1

--- day4/day1.py
+++ day4/day1.py
@@ -4,7 +4,11 @@ demo_text = """1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet"""
+# with open('...') as f:
 
 for line in io.StringIO(demo_text):
     print(line, end='')
-    
+
+# Take first and last digit from each line,
+# sum the values
+# result should be: 142
